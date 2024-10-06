d34eb909c5cc59f2609207c0a0f1d55f09ebff48
JAMIE_HARRIS <JAMIE.HARRIS1991@HOTMAIL.CO.UK>
2024-10-06 19:36:34 +0100

    [ADD]: Add functionality to the calculate button

---
+++ assets/javascript/script.js
@@ -0,0 +1,5 @@
+document.getElementById('calculate-button').addEventListener('click', calculateTakeHomePay);
+
+function calculateTakeHomePay() {
+    alert('Please enter a valid gross income.');
+}

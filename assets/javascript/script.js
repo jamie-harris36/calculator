5004e9829245424fd34f8206fad72dd7003c846f
JAMIE_HARRIS <JAMIE.HARRIS1991@HOTMAIL.CO.UK>
2024-10-07 01:01:28 +0100

    [ADD]: Add code to calculate income tax

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,7 +8,7 @@ function calculateTakeHomePay() {
     }
 
 
-    // Personal Allowance
+    // Personal Allowance - income below this amount is tax free
     let personalAllowance = 12570;
 
 
@@ -21,4 +21,26 @@ function calculateTakeHomePay() {
     // Income tax brackets
     const basicRateLimit = 50270;
     const higherRateLimit = 125140;
+
+
+    // Code to calculate income tax
+
+        // If salary is less than or equal to £50,270
+    if (grossIncome <= basicRateLimit) {
+        // Then taxable income is taxed at 20%
+        incomeTax = taxableIncome * 0.20;
+
+        // If salary is less than or equal to £125,140
+    } else if (grossIncome <= higherRateLimit) {
+        // Then taxable income is taxed at 20% of £37,700 (£50,270 - £12,570 tax-free allowance), + 40% of remaining income.
+        incomeTax = ((basicRateLimit - personalAllowance) * 0.20) + 
+        ((grossIncome - basicRateLimit) * 0.40);
+
+        // If salary is more than £125,140
+    } else {
+        // Then taxable income is taxed at 20% of £37,700, + 40% of income between £50,270 and £125,140, and 45% of remaining income.
+        incomeTax = ((basicRateLimit - personalAllowance) * 0.20) + 
+        ((higherRateLimit - basicRateLimit) * 0.40) + 
+        ((grossIncome - higherRateLimit) * 0.45);
+    }
 }
